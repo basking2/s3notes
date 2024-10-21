aefcacd76015b4e3a5bd69051db43d1c79030bed
Sam Baskinger <basking2@yahoo.com>
2024-10-21 13:23:14 -0500

    feat(markdown): Add markdown viewing.

--- src/renderviews/MarkdownRenderView.js
+++ src/renderviews/MarkdownRenderView.js
@@ -1,8 +1,9 @@
+import { marked } from 'marked'
 
 export default function MarkdownRenderView(params={}) {
     const { text } = params
 
-    const html = <pre>{text}</pre>
+    const html = marked.parse(text)
 
     return <div dangerouslySetInnerHTML={{__html: html}}></div>
 }
