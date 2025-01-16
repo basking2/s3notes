600189c8ab45b14bbdf85135d0e169be6b948882
Sam Baskinger <basking2@yahoo.com>
2025-01-15 20:29:43 -0600

    feat(AsciidocRenderView.js): Remove footer from rendered AsciiDoc pages.

--- src/renderviews/AsciidocRenderView.js
+++ src/renderviews/AsciidocRenderView.js
@@ -4,7 +4,7 @@ export default function AsciidocRenderView(params={}) {
     const { text } = params
 
     const asciidoctor = Asciidoctor()
-    const html = asciidoctor.convert(text, { standalone: true })
+    const html = asciidoctor.convert(text, { standalone: true, attributes: { nofooter: true }})
 
     return <div dangerouslySetInnerHTML={{__html: html}}></div>
 }
